685ec30fd94ac919e0f125a8dab7123493fd4911
April Rieger <april.rieger@yahoo.com>
2023-04-21 00:49:02 -0700

    default create_relationships & create_file_sets, removed the importer controller, removed the decorator, removed the overridden partial as unecessary now

--- app/parsers/bulkrax/oai_ia_parser.rb
+++ app/parsers/bulkrax/oai_ia_parser.rb
@@ -1,3 +1,6 @@
+# OVERRIDE: Bulkrax 4.4.1 to add collection title, noted below.
+# OVERRIDE: Bulkrax 4.4.1 to get a patch from Bulkrax 5.x.x for create_objects, noted below.
+# TODO: remove create_objects method when upgrading to Bulkrax 5.x.x
 module Bulkrax
   # Parser for the Internet Archive OAI-PMH Endpoint
   class OaiIaParser < OaiDcParser
@@ -5,21 +8,16 @@ module Bulkrax
       OaiIaEntry
     end
 
-    # def collection_entry_class
-    #   # OaiSetEntry # default from application_parser.rb
-    # end
-
-    # @abstract Subclass and override {#create_file_sets} to implement behavior for the parser.
-    def create_file_sets
-      # create_objects(['file_set'])
-    end
-
-    # @abstract Subclass and override {#create_relationships} to implement behavior for the parser.
-    def create_relationships
-      # create_objects(['relationship'])
-    end
-
+    def create_file_sets; end
+    def create_relationships; end
     def file_set_entry_class; end
+
+    # override create_objects to get the patch from https://github.com/samvera-labs/bulkrax/pull/713
+    def create_objects(types = [])
+      types.each do |object_type|
+        send("create_#{object_type.pluralize}")
+      end
+    end
 
     def create_collections
       metadata = {
@@ -27,29 +25,23 @@ module Bulkrax
         collection_type_gid: Hyrax::CollectionType.find_or_create_default_collection_type.gid
       }
 
-      # OVERRIDE Bulkrax 4.4.1 to add collection title
       collections.each_with_index do |set, index|
         next unless collection_name == 'all' || collection_name == set.spec
         unique_collection_identifier = importerexporter.unique_collection_identifier(set.spec)
 
+        # add collection title
         metadata[:title] = [parser_fields['collection_title'] || set.name]
         metadata[work_identifier] = [unique_collection_identifier]
 
         new_entry = collection_entry_class.where(importerexporter: importerexporter,
                                                  identifier: unique_collection_identifier,
                                                  raw_metadata: metadata)
-                                                 .first_or_create!
+                                          .first_or_create!
         # perform now to ensure this gets created before work imports start
         ImportCollectionJob.perform_now(new_entry.id, importerexporter.current_run.id)
         increment_counters(index, collection: true)
       end
     end
 
-    def create_objects(types = [])
-      types_array ||= Bulkrax::Importer::DEFAULT_OBJECT_TYPES
-      types.each do |object_type|
-        send("create_#{object_type.pluralize}")
-      end
-    end
   end
 end
