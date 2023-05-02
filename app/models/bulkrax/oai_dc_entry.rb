ec0e5582a7b097350609b3bb441ab7d414e43b01
Alisha Evans <alisha@scientist.com>
2023-05-02 16:07:20 -0500

    move the Bulkrax::ImportBehavior#add_collections override from OaiIaEntry to OaiDcEntry (#291)

--- app/models/bulkrax/oai_dc_entry.rb
+++ app/models/bulkrax/oai_dc_entry.rb
@@ -4,5 +4,13 @@ module Bulkrax
     def self.matcher_class
       Bulkrax::AtlaOaiMatcher
     end
+
+    # OVERRIDE: v4.4.2 Bulkrax::ImportBehavior#add_collections
+    def add_collections
+      return if self.find_collection_ids.blank?
+
+      # we need this mapping to exist so that Bulkrax::ImportBehavior#build_for_importer calls #parent_jobs
+      self.parsed_metadata[related_parents_parsed_mapping] = self.find_collection_ids
+    end
   end
 end
