65c0b16c78ae73ebdc739be66883992491408d1a
April Rieger <april.rieger@yahoo.com>
2023-05-16 15:36:43 -0700

    Updates to add echo statements in the scri[t with set -x

--- ops/bin/db-migrate-seed.sh
+++ ops/bin/db-migrate-seed.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
-set -e
+set -x
 
+echo "Waiting for database to be ready..."
 db-wait.sh "$DB_HOST:$DB_PORT"
+
+echo "Creating database..."
 bundle exec rails db:create
+
+echo "Running migrations..."
 bundle exec rails db:migrate
 
 if [ "$FCREPO_HOST" ]; then
+  echo "Waiting for FCREPO to be ready..."
   db-wait.sh "$FCREPO_HOST:$FCREPO_PORT"
 fi
+
+echo "Waiting for SOLR to be ready..."
 db-wait.sh "$SOLR_HOST:$SOLR_PORT"
 
+echo "Seeding database..."
 bundle exec rails db:seed
