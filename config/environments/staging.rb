ed44e7199bcf43d0787accb7974b03c9a8bc8c31
Rob Kaufman <rob@notch8.com>
2023-08-10 15:18:29 -0700

    update mailer settings

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = { from: 'no-reply@dl.atla.com' }
+  config.action_mailer.default_options = { from: 'noreply@atlamail.org' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
